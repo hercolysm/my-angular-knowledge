d1880c13329808efbe330496b813e055179a8d3b
Hercolys Moraes Araujo <hercolysmoraes@live.com>
2026-08-08 21:54:07 -0300

    Sync files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./components/login/login').then(m => m.Login)
+    }
+];
